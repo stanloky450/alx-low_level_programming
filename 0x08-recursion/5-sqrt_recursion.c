309156e63ebf8b8623b7f8895c1e60ea82a208c3
stanloky450 <backlinkreator@gmail.com>
2023-10-25 15:55:25 -0700

    Recursion project

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,46 +1,33 @@
 #include "main.h"
-
+int actual_sqrt_recursion(int n, int i);
 /**
- * _sqrt_recursion - Returns the natural square root of a number.
- * @n: The number for which to calculate the square root.
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
  *
- * Return: The natural square root of n, or -1 if there's no natural square root.
+ * Return: the resulting square root
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-    {
-        return -1;  
-    }
-    else if (n == 0 || n == 1)
-    {
-        return n;   
-    }
-    else
-    {
-        return find_sqrt(n, 1);  
-    }
+
+	if (n < 0)
+	return (-1);
+	return (actual_sqrt_recursion(n, 0));
 }
 
 /**
- * find_sqrt - Helper function to find the square root using recursion.
- * @n: The number for which to calculate the square root.
- * @guess: The current variable for the square root.
+ * actual_sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
  *
- * Return: The square root of n.
+ * Return: the resulting square roo
  */
-int find_sqrt(int n, int guess)
+int actual_sqrt_recursion(int n, int i)
 {
-    if (guess * guess == n)
-    {
-        return guess;  
-    }
-    else if (guess * guess > n)
-    {
-        return -1;  
-    }
-    else
-    {
-        return find_sqrt(n, guess + 1);  
-    }
+	if (i * i > n)
+	return (-1);
+	if (i * i == n)
+	return (i);
+return (actual_sqrt_recursion(n, i + 1));
 }
+
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,35 +1,33 @@
 #include "main.h"
 
+int actual_prime(int n, int i);
+
 /**
- * is_prime_number - Checks if a number is prime.
- * @n: The integer to be checked for primality.
+ * is_prime_number - says if an integer is a prime number or not
+ * @n: number to evaluate
  *
- * Return: 1 if the number is prime, 0 otherwise.
+ * Return: 1 if n is a prime number, 0 if not
  */
 int is_prime_number(int n)
 {
-    if (n <= 1)
-    {
-        return 0;
-    }
-
-    if (n <= 3)
-    {
-        return 1;
-    }
-
-    if (n % 2 == 0 || n % 3 == 0)
-    {
-        return 0;
-    }
-
-    for (int i = 5; i * i <= n; i += 6)
-    {
-        if (n % i == 0 || n % (i + 2) == 0)
-        {
-            return 0;
-        }
-    }
-
-    return 1;
+	if (n <= 1)
+	return (0);
+	return (actual_prime(n, n - 1));
 }
+
+/**
+ * actual_prime - calculates if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+	return (1);
+	if (n % i == 0 && i > 0)
+	return (0);
+	return (actual_prime(n, i - 1));
+}
+

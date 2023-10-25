6723fb605704dc3de542b7a67b147705841c62a4
stanloky450 <backlinkreator@gmail.com>
2023-10-25 15:31:18 -0700

    Recursion project

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,10 +8,12 @@ void _puts_recursion(char *s)
 {
     if (*s == '\0')
     {
+        /* When we reach the end of the string, print a new line. */
         putchar('\n');
     }
     else
     {
+        /* Print the current character and recursively call _puts_recursion on the rest of the string. */
         putchar(*s);
         _puts_recursion(s + 1);
     }
